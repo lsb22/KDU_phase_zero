a8501cfc11f39f4095bd1ec1be358f1cf87da626
lsb <likith15600@gmail.com>
2025-11-22 17:13:02 +0530

    take input through buffered reader

--- com/kickdrum/javaConcertTicketSystem/UserNameChecker.java
+++ com/kickdrum/javaConcertTicketSystem/UserNameChecker.java
@@ -1,23 +1,48 @@
 package com.kickdrum.javaConcertTicketSystem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class UserNameChecker {
     public static void main(String[] args) {
-        if (args.length < 2) {
+        String[] userName = null;
+
+        // read username as a single string
+        // split the string and store usernames in array
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.print("Enter your UserName twice seperated by a comma: ");
+            userName = (br.readLine()).split(",");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // check if length in not equal to 2
+        if (userName.length != 2) {
             System.out.println("Please Enter your UserName twice for ticket confirmation!");
             System.exit(-1);
         }
-        System.out.println("Length of the First String: " + args[0].length());
-        System.out.println("Length of the Second String: " + args[1].length());
+        userName[0] = userName[0].trim();
+        userName[1] = userName[1].trim();
+
+        // check if usernames are empty
+        if (userName[0].length() == 0 || userName[1].length() == 0) {
+            System.out.println("UserName shouldn't be empty!");
+            System.exit(-1);
+        }
+
+        System.out.println("Length of the First String: " + userName[0].length());
+        System.out.println("Length of the Second String: " + userName[1].length());
 
         // checks if strings length are same
-        if (args[0].length() == args[1].length()) {
+        if (userName[0].length() == userName[1].length()) {
             System.out.println("Both Strings length Match: True");
         } else {
             System.out.println("Both Strings length Match: False");
         }
 
         // checks if strings are same
-        if (args[0].equals(args[1])) {
+        if (userName[0].equals(userName[1])) {
             System.out.println("Both Strings Match: True");
         } else {
             System.out.println("Both Strings Match: False");
