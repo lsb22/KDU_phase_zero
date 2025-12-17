2fbdee820bfa6d188ab170721841a9dcff11b926
lsb <likith15600@gmail.com>
2025-12-17 13:18:27 +0530

    add tests for findBookById method

--- library/src/test/java/com/kickdrum/library/service/BookServiceTest.java
+++ library/src/test/java/com/kickdrum/library/service/BookServiceTest.java
@@ -10,6 +10,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 @ExtendWith(MockitoExtension.class)
 class BookServiceTest {
     @Mock
@@ -41,5 +43,32 @@ class BookServiceTest {
         });
 
         Assertions.assertEquals("Book Life already exists",runtimeException.getMessage());
+    }
+
+    @Test
+    void findBookByIdShouldSuccessfullyReturnBookWithGivenId() {
+        Book expectedBook = new Book("Book1","Ling",1000.000,"Kannada");
+        expectedBook.setId(5);
+
+        Mockito.when(bookRepository.findById(5)).thenReturn(Optional.of(expectedBook));
+        Book actualBook = bookService.findBookById(5);
+
+        Assertions.assertEquals(expectedBook.getId(),actualBook.getId());
+        Assertions.assertEquals(expectedBook.getTitle(),actualBook.getTitle());
+        Mockito.verify(bookRepository,Mockito.times(1)).findById(5);
+    }
+
+    @Test
+    void findBookByIdShouldThrowRuntimeExceptionForInvalidIds() {
+        int bookId = 5;
+        Mockito.when(bookRepository.findById(bookId)).thenReturn(Optional.empty());
+
+        // I expect this error to happen when executing this particular code,
+        // so catch it and return the exception object so that I can inspect it
+        RuntimeException runtimeException = Assertions.assertThrows(RuntimeException.class, () -> {
+            bookService.findBookById(bookId);
+        });
+
+        Assertions.assertEquals("Book with id: "+bookId+" not found",runtimeException.getMessage());
     }
 }
