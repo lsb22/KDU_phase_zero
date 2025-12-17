2e0e68624f1b1e5d2159f17e72bf899d964fe330
lsb <likith15600@gmail.com>
2025-12-17 14:21:17 +0530

    correct error handling logic

--- library/src/main/java/com/kickdrum/library/controller/BookController.java
+++ library/src/main/java/com/kickdrum/library/controller/BookController.java
@@ -42,7 +42,7 @@ public class BookController {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String,String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String,String> errors = new HashMap<String,String>();
-        errors.put("timestamp",LocalDateTime.now().toString());
+        errors.put("timestamp",LocalDateTime.now().toLocalDate().toString());
         errors.put("error","Validation failed");
         // HttpStatus.BAD_REQUEST.value(): returns code for Bad Request : 400
         // Bad Request indicates client error
@@ -61,14 +61,15 @@ public class BookController {
     public ResponseEntity<Map<String,String>> handleRuntimeExceptions(Exception ex){
         Map<String,String> errors = new HashMap<String,String>();
         errors.put("message",ex.getMessage());
-        errors.put("timestamp", LocalDateTime.now().toString());
+        errors.put("timestamp", LocalDateTime.now().toLocalDate().toString());
 
-        if(ex.getMessage().contains("already exists")) {
+        if(ex.getMessage().contains("Duplicate entry")) {
             // HttpStatus.CONFLICT.value(): returns code for Conflict : 409
             // Conflict indicates conflict with the current state of the target resource.
             errors.put("status", Integer.toString(HttpStatus.CONFLICT.value()));
+            errors.put("message","Don't provide duplicate Entries");
             errors.put("error","Duplicate Entry");
-            return ResponseEntity.badRequest().body(errors);
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(errors);
         } else if(ex.getMessage().contains("not found")) {
             errors.put("status",Integer.toString(HttpStatus.NOT_FOUND.value()));
             errors.put("error","Book not found");
