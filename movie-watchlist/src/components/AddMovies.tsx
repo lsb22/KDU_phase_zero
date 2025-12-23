0069ac5d5329ea93be827bdebcf510fcd6c86e8d
lsb <likith15600@gmail.com>
2025-12-23 16:09:41 +0530

    add shadcn dropdown menu component

--- movie-watchlist/src/components/AddMovies.tsx
+++ movie-watchlist/src/components/AddMovies.tsx
@@ -1,5 +1,34 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 const AddMovies = () => {
-  return <div>Add Movies</div>;
+  return (
+    <div>
+      <Input type="text" placeholder="Enter your movie name" />
+      <div>
+        <DropdownMenu>
+          <DropdownMenuTrigger>Rating</DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <Button className="bg-yellow-700">Add to Watchlist</Button>
+    </div>
+  );
 };
 
 export default AddMovies;
