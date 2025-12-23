bcd27f171429f0d2183c5d54fbbeace8ada354c7
lsb <likith15600@gmail.com>
2025-12-23 15:34:26 +0530

    create grid layot

--- movie-watchlist/src/App.tsx
+++ movie-watchlist/src/App.tsx
@@ -1,5 +1,21 @@
 const App = () => {
-  return <div>Hello hello chammak challo</div>;
+  return (
+    <div className="wrapper1">
+      <h1 className="app-title">Movie WatchList</h1>
+      <div className="wrapper2">
+        <div className="part1">
+          <h2 className="part1-title">Add your favourite Movie</h2>
+          <div className="">Add movies Component</div>
+          <div className="">Movies in WatchList: 0</div>
+          <button className="">Clear Watchlist</button>
+        </div>
+        <div className="part2">
+          <input type="text" placeholder="Search your favourite movies here!" />
+          <div className="">Display movies component</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
